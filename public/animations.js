c923b9d0cf7893126f2a88b8d5339609ee968974
ThomasScheepers <thomas@scheepers.com>
2023-03-22 22:22:10 +0100

    progress

--- public/animations.js
+++ public/animations.js
@@ -4,8 +4,18 @@ gsap.from(".player-card", {
     y: 40,
     ease: "power1.inOut",
     stagger: {
-      // grid: [5,23],
-      from: "start",
-      amount: 1.5
-    }
-  });
+    from: "start",
+    amount: .5
+  }
+});
+
+gsap.from("tr", {
+  duration: .02,
+  scale: 0.0,
+  y: 40,
+  ease: "power1.inOut",
+  stagger: {
+  from: "start",
+  amount: .5
+}
+});
