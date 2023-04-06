06989bd905990c2376a290478af02c0dbbd05ab1
ThomasScheepers <thomas@scheepers.com>
2023-04-06 09:56:19 +0200

    nodemon off

--- public/animations.js
+++ public/animations.js
@@ -19,3 +19,14 @@ gsap.from("tr", {
   amount: .5
 }
 });
+
+gsap.from(".long-player-card", {
+  duration: .02,
+  scale: 0.0,
+  y: 40,
+  ease: "power1.inOut",
+  stagger: {
+  from: "start",
+  amount: 2
+}
+});
